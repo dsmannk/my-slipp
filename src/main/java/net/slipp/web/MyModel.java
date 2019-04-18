2b402aa3c91330d649548c7d347731a5b10ff8bf
dsmannk <dsmannk@gmail.com>
2019-04-18 17:39:30 +0900

    [20190418_dsmannk]html template 추가

---
+++ src/main/java/net/slipp/web/MyModel.java
@@ -0,0 +1,18 @@
+package net.slipp.web;
+
+public class MyModel {
+	
+	private String name;
+	
+	public MyModel(String name) {
+		
+		this.name = name;
+	}
+	
+	public String getName() {
+		
+		return this.name;
+	}
+	
+	
+}
--- src/main/java/net/slipp/web/WelcomeController.java
+++ src/main/java/net/slipp/web/WelcomeController.java
@@ -1,13 +1,41 @@
 package net.slipp.web;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 	
-	@PostMapping("/helloworld")
+	
+	@GetMapping("/helloworld")
+	public String welcome(Model model) {
+		//List<MyModel> repo = new ArrayList<MyModel>();
+		List<MyModel> repo = Arrays.asList(new MyModel("dsmannk"), new MyModel("sams"));
+		
+		//System.out.println("repo = ", repo);
+		
+		model.addAttribute("repo", repo);
+		return "welcome";
+	}
+	
+	/*
+	@GetMapping("/helloworld3")
+	public String welcome(Model model) {
+		
+		model.addAttribute("name", "dsmannk");
+		model.addAttribute("value", 10000);
+		model.addAttribute("taxed_value", 30);
+		model.addAttribute("in_ca", true);
+		return "welcome";
+	}
+	*/
+	
+	
+	@GetMapping("/helloworld2")
 	public String welcome(String name, int age, Model model) {
 		System.out.println("name : " + name);
 		model.addAttribute("name", name);
