66975811a18ac96c0ee556e3ee4a65f5b0f4a587
dsmannk <dsmannk@gmail.com>
2019-04-22 08:38:51 +0900

    [20190422_dsmannk]html, 개인정보 수정, 회원가입 기능 구현

---
+++ src/main/java/net/slipp/domain/User.java
@@ -0,0 +1,61 @@
+package net.slipp.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(nullable=false, length=20)
+	private String userId;
+	
+	private String password;
+	private String name;
+	private String email;
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void update(User newUser) {
+
+		this.password = newUser.password;
+		this.name = newUser.name;
+		this.email = newUser.email;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "User [id =" + id + "userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+	}
+
+
+}
