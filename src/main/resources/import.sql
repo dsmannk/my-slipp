e663df51c28f777d32e39483173d2fd282f65bb0
dsmannk <dsmannk@gmail.com>
2019-04-25 17:28:45 +0900

    [20190425_dsmannk]중복 코드 제거

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
 insert into USER (EMAIL, NAME, PASSWORD, USER_ID) values ('dsmannk@naver.com','sams','test','dsmannk');
 insert into USER (EMAIL, NAME, PASSWORD, USER_ID) values ('nkmands@gmail.com','sami','test','nkmands');
+
+
+insert into QUESTION (id, writer_id, title, contents, create_date) values (1, 1, '안녕하세요 Ruby on Rails는 Play좋은 경헙이 될것입니다.', 'Ruby on Rails(루비온레일즈 RoR)', CURRENT_TIMESTAMP());
+insert into QUESTION (id, writer_id, title, contents, create_date) values (2, 2, '즐거운 자바', '자바를 처음해보시나요?그럼 이걸 해보세요', CURRENT_TIMESTAMP());
